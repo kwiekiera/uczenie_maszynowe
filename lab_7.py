748c89979ea2b7dd2241a2e23b60ba13cb1a715a
kwiekiera <kamila.wiekiera@edu.uekat.pl>
2022-11-15 17:09:03 +0100

    message

--- lab_7.py
+++ lab_7.py
@@ -1,6 +1,8 @@
 import pytesseract
 import cv2
 
+pytesseract.pytesseract.tesseract_cmd = r'C:/Users/kamil/AppData/Local/Tesseract-OCR/tesseract.exe'
+
 def showText(name):
     image = cv2.imread(name)
     print(pytesseract.image_to_string(image))
